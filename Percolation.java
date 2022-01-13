04e510d73e021ad731f31a9d2d16948459ae4f7b
Cicek <cicek.ozk@gmail.com>
2022-01-13 15:45:33 +0100

    Adjusted some spacing

--- Percolation.java
+++ Percolation.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     public static void main(String[] args) {
-
     }
     private int n;
     private boolean[][] opened;
@@ -21,7 +20,6 @@ public class Percolation {
         // 0 = virtual top, n*n-1 = virtual bottom, thus the +2
         union = new WeightedQuickUnionUF(n * n + 2);
     }
-
     // top left row is supposed to be (1|1), not (0|0), so we check the bounds
     public void checkBounds(int row, int col) {
         if (row < 1 || col < 1 || row > this.n || col > this.n) {
@@ -32,14 +30,12 @@ public class Percolation {
         checkBounds(row, col);
         return (row - 1) * this.n + col;
     }
-
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         checkBounds(row, col);
         if (!(isOpen(row, col))) {
             opened[row - 1][col - 1] = true;
             numberOpened++;
-
             // if site on first row is opened, it's connected to the virtual top
             if (row == 1) {
                 union.union(xyTo1D(row, col), 0);
@@ -64,7 +60,6 @@ public class Percolation {
             }
         }
     }
-
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         checkBounds(row, col);
@@ -85,7 +80,6 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
         return union.find(0) == union.find(n * n - 1);
-
     }
 
     // // test client (optional)
